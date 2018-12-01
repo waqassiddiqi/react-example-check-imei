6e6b57757ba04807b0aa8745648226777ca2006a
Waqas Hussain Siddiqui <waqas.siddiqi@hotmail.com>
2018-12-01 11:43:27 +0800

    added mock api call

--- src/device.js
+++ src/device.js
@@ -3,18 +3,39 @@ import { Card, Icon, Avatar } from "antd";
 import { Col, Row } from "antd";
 import "./styles.css";
 const { Meta } = Card;
+const API = "https://5c01f8d2d526f900134722f7.mockapi.io/api/v1/devices/";
 
 class Device extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { device: [], imei: "" };
+    this.state = { devices: [], imei: "", isLoading: false };
   }
 
   componentWillReceiveProps(props) {
     const { imei } = this.props;
     if (props.imei !== imei) {
-      alert("new");
+      this.setState({ isLoading: true });
+      fetch(API + imei)
+        .then(response => {
+          alert(response);
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("An error has occured");
+          }
+        })
+        .then(data => this.setState({ devices: data, isLoading: false }))
+        .catch(error => this.setState({ isLoading: false }));
     }
+  }
+
+  getConditionalProps() {
+    const props = {};
+    if (this.state.isLoading) {
+      props.loading = "loading";
+    }
+
+    return props;
   }
 
   render() {
@@ -24,6 +45,7 @@ class Device extends React.Component {
           <Col span={8} />
           <Col span={8}>
             <Card
+              {...this.getConditionalProps()}
               hoverable
               cover={
                 <img
